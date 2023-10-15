3bedba62df433e4aee7ed7dbb911843c81ddc705
poojanaohas <147986891+poojanaohas@users.noreply.github.com>
2023-10-15 19:01:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94774395441', 'me', true],
+  ['94774395441', 'SL DRAGON', true], 
   [''] 
 ] //Number of owners
 
